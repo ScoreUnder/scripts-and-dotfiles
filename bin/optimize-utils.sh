df5c0dc77a4a821766ab41bb45d95ceed0c730da
Score_Under <seejay.11@gmail.com>
2015-03-06 16:54:39 +0000

    Add gif optimization to optdl and move common code out into another file

---
+++ bin/optimize-utils.sh
@@ -0,0 +1,36 @@
+optimize_result() {
+    f=$1; size=$2; newsize=$3
+    if [ "$newsize" = 0 ]; then
+        printf '%s: Optimization failed\n' "$f"
+        false
+    elif [ "$size" -gt "$newsize" ]; then
+        printf '%s: %d → %d bytes (%s%% saved)\n' "$f" "$size" "$newsize" "$(printf 'scale=1;100*(%s-%s)/%s\n' "$size" "$newsize" "$size"|bc)"
+        true
+    elif [ "$size" = "$newsize" ]; then
+        printf '%s: No change in size\n' "$f"
+        true
+    else
+        printf '%s: Cannot optimize (%s < %s)\n' "$f" "$size" "$newsize"
+        false
+    fi
+}
+
+# Changes traps. Use only as the last function in the script or as a subshell.
+# Usage: optimize_with_func optimize "$@"
+# where optimize is a shell function taking 2 parameters (original file, file to write to)
+optimize_with_func() {
+    local tmpfile
+    tmpfile=
+    cleanup() { ret=$?; rm -f -- "$tmpfile"; exit "$ret"; }
+    trap cleanup EXIT HUP INT TERM
+    tmpfile=$(mktemp)
+
+    optimize=$1; shift
+    for f do
+        size=$(stat -c%s -- "$f") || continue
+        "$optimize" "$f" "$tmpfile" || continue
+        newsize=$(stat -c%s -- "$tmpfile") || continue
+        optimize_result "$f" "$size" "$newsize" &&
+            cp -- "$tmpfile" "$f"
+    done
+}
