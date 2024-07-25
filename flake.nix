763d762d3b4a6e4a54c126ccc61200bb66af48f3
Score_Under <seejay.11@gmail.com>
2024-07-25 18:44:57 +0100

    Add flake/shell.nix
    I am still very new to this so please comment if this doesn't look quite right!

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "score_under's dotfiles";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        }
+      );
+}
